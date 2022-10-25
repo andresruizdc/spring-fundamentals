cab2cd0569fc8e68ff405a50cdb5d4f5a9ba9da0
andrdc <andrsruiz@gmail.com>
2022-10-25 14:51:22 -0500

    Add Hello Controller and View

---
+++ src/main/java/com/andrdc/springfundamentals/controllers/Hello.java
@@ -0,0 +1,15 @@
+package com.andrdc.springfundamentals.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Hello {
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("user", name);
+        return "hello";
+    }
+}
