b9cf3e7d7b766cda15c6840f3bb7f11e434cdf39
andrdc <andrsruiz@gmail.com>
2022-10-25 17:31:51 -0500

    Add mock mvc tests

---
+++ src/test/java/com/andrdc/springfundamentals/controllers/HelloMockMVCTest.java
@@ -0,0 +1,38 @@
+package com.andrdc.springfundamentals.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(Hello.class)
+public class HelloMockMVCTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void autoWiringWorks() {
+        assertNotNull(mvc);
+    }
+
+    @Test
+    public void helloWithoutName() throws Exception {
+        mvc.perform(get("/hello").accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andExpect(view().name("hello"))
+                .andExpect(model().attribute("user", "World"));
+    }
+
+    @Test
+    public void helloWithName() throws Exception {
+        mvc.perform(get("/hello").param("name", "Gizmo").accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andExpect(view().name("hello"))
+                .andExpect(model().attribute("user", "Gizmo"));
+    }
+}
