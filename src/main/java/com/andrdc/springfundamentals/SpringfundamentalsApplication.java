04861de461a2a201143e8f71fc0cf3721b277d60
andrdc <andrsruiz@gmail.com>
2022-10-25 14:18:26 -0500

    Initial commit 2

---
+++ src/main/java/com/andrdc/springfundamentals/SpringfundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.andrdc.springfundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringfundamentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrdc/springfundamentals/SpringfundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrdc.springfundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
